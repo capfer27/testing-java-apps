45d7e86dba1cbcff0e45c32d73425ed6f85f3252
capfer27 <capfer27@gmailcom>
2025-11-19 18:36:03 +0400

    not overriding the password

--- usersservice/src/test/java/com/capfer/usersservice/controllers/UsersControllerTestWithTestContainers.java
+++ usersservice/src/test/java/com/capfer/usersservice/controllers/UsersControllerTestWithTestContainers.java
@@ -1,5 +1,6 @@
 package com.capfer.usersservice.controllers;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -21,8 +22,8 @@ public class UsersControllerTestWithTestContainers {
     @Container
     private static final PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:17.7")
             .withDatabaseName("test_app")
-            .withUsername("postgres")
-            .withPassword("");
+            .withUsername("postgres");
+            //.withPassword("");
 
     // Override the properties defined in the application.properties file.
     @DynamicPropertySource
@@ -30,5 +31,10 @@ public class UsersControllerTestWithTestContainers {
         propertyRegistry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
         propertyRegistry.add("spring.datasource.username", postgreSQLContainer::getUsername);
         propertyRegistry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+    }
+
+    @Test
+    void contextLoads() {
+        System.out.println("IsCreated: " + postgreSQLContainer.isCreated());
     }
 }
