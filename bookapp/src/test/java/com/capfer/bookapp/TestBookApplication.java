2c6ebd0e779f1236b5e31974bfc2481a17ca2a59
capfer27 <capfer27@gmailcom>
2025-11-24 21:35:00 +0400

    disable unused tests

--- bookapp/src/test/java/com/capfer/bookapp/TestBookApplication.java
+++ bookapp/src/test/java/com/capfer/bookapp/TestBookApplication.java
@@ -1,7 +1,9 @@
 package com.capfer.bookapp;
 
+import org.junit.jupiter.api.Disabled;
 import org.springframework.boot.SpringApplication;
 
+@Disabled
 public class TestBookApplication {
 
 	public static void main(String[] args) {
